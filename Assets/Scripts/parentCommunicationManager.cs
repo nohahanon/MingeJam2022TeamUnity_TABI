e9baf3cf1de7fbdc17778bec1771649a408e34da
nohahanon <haihuiliyuyuan@gmail.com>
2022-12-25 14:26:13 +0900

    会話まわりの修正とgrassWorldの装飾

---
+++ Assets/Scripts/parentCommunicationManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class parentCommunicationManager : MonoBehaviour
+{
+
+    public GameObject communicationPanel;
+    public Text nameInCommunicationPanel, serifsInCommunicationPanel;
+    public float textSpeed = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        communicationPanel = GameObject.FindWithTag("communicationPanel");
+        nameInCommunicationPanel = GameObject.FindWithTag("nameInCommunicationPanel").GetComponent<Text>();
+        serifsInCommunicationPanel = GameObject.FindWithTag("serifsInCommunicationPanel").GetComponent<Text>();
+        communicationPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
